47cc4df3aa9e9688fbcd3f1b1031c5d68f633b89
Garen <geejay412@gmail.com>
2023-12-17 17:17:36 +0000

    Menu UI Implementation
    -Implemented UI as canvas layers overlaying the game scene
    
    -Start button sets the canvas to inactive, nothing else
    
    -Credits button sets main menu to inactive and credits menu to active
    --Back button (credits menu) reverts the credits button change (activated main, deactivates credits)
    
    -Quit button runs script 'menus' to run function 'ExitGame', this *should* work on a built version
    
    -I tried not to touch anything unrelated to the menus but i did build the menu canvas on the main game scene, this could cause issues when merging but duplicating the scene felt like a bit much since this sort of menu is very simple.

---
+++ Assets/Scripts/Menus.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menus : MonoBehaviour
+{
+
+void Start()
+{
+
+}
+
+void Update()
+{
+
+}
+
+public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exiting");
+    }
+    
+}
